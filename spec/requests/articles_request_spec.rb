6912fb0b174727211b76bfb731e6d9250e706e42
Christophe Hartmann <chartmann.35@gmail.com>
2020-12-28 16:48:01 +0100

    feat: add spec for users and articles

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -3,4 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles', type: :request do
+
+  it 'show articles' do
+    category = FactoryBot.create(:category)
+    get "/articles/#{category.id}"
+
+    expect(response.status).to eq(200)
+  end
 end
